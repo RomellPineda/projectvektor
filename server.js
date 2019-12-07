4a77cbde2f4f1a0b84442448e2724a12181f4813
RomellPineda <38547179+RomellPineda@users.noreply.github.com>
2019-12-06 16:49:31 -0800

    Update server.js
    Add process.env port

--- server.js
+++ server.js
@@ -10,13 +10,11 @@ app.use(express.static( __dirname + '/public/dist/public' ));
 require('./server/config/mongoose.js');
 require('./server/config/routes.js')(app);
 
-// app.listen(8000, function() {
-//     console.log("/// Standing by on 8000");
-// })
+const PORT = process.env.PORT || 5000;
 
 // Socket.io
-const server = app.listen(5000, () => {
-    console.log('/// Standing by on 5000')
+const server = app.listen(PORT, () => {
+    console.log(`Standing by on ${PORT}`)
 });
 const io = require('socket.io')(server);
 users = [];
